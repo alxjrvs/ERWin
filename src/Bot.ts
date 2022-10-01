f94205393396b24dea9b21123eb59a6ce466bb0b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-10-01 17:53:42 -0400

    initial Style setup

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,6 +1,6 @@
-import { Client } from "discord.js"
+import { Client } from 'discord.js'
 
-console.log("Bot is starting...")
+console.log('Bot is starting...')
 
 const client = new Client({
   intents: []
