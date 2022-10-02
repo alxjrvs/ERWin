3c50b858ad69d231ae20fb2ff7eee2815db0c764
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-10-01 20:59:11 -0400

    Minor working prototype

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,9 +1,48 @@
-import { REST, SlashCommandBuilder, Routes } from 'discord.js'
+import { REST, SlashCommandBuilder, Routes, SlashCommandStringOption } from 'discord.js'
 import { config } from 'dotenv'
 config()
 
+const rex = new SlashCommandBuilder()
+  .setName('mecatol')
+  .setDescription('Replies with rex')
+SlashCommandStringOption
+const activate = new SlashCommandBuilder()
+  .setName('activate')
+  .setDescription('Schedule and announce a new game of Twilight Imperium')
+  .addUserOption(option =>
+    option.setName('host')
+      .setDescription('The @handle of the Host of the event')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('name')
+      .setDescription('Name of this game (Will become Channel and @Role)')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('date')
+      .setDescription('The Date of the event (MM/DD/YY)')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('start_time')
+      .setDescription('Suggested start time (HH:MM AM/PM')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('end_time')
+      .setDescription('Suggested end time (HH:MM AM/PM')
+      .setRequired(true))
+  .addStringOption(option =>
+    option.setName('title')
+      .setDescription('Catchy Title'))
+  .addStringOption(option =>
+    option.setName('subtitle')
+      .setDescription('Catchy SubTitle'))
+  .addStringOption(option =>
+    option.setName('description')
+      .setDescription('Catchy Description'))
+
+
 const commands = [
-  new SlashCommandBuilder().setName('mecatol').setDescription('Replies with pong!'),
+  rex,
+  activate,
 ]
   .map(command => command.toJSON())
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, GatewayIntentBits } from 'discord.js'
 import { config } from 'dotenv'
 
 config()
@@ -9,13 +9,46 @@ client.once('ready', () => {
   console.log('Ready!')
 })
 
-client.on('interactionCreate', async interaction => {
+// Slash Commands
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return
 
   const { commandName } = interaction
 
   if (commandName === 'mecatol') {
     await interaction.reply('rex')
+  }
+  if (commandName === 'activate') {
+    const options = {
+      host: interaction.options.getUser('host'),
+      name: interaction.options.getString('name'),
+      date: interaction.options.getString('date'),
+      startTime: interaction.options.getString('start_time'),
+      endTime: interaction.options.getString('end_time'),
+      title: interaction.options.getString('title'),
+      subtitle: interaction.options.getString('subtitle'),
+      description: interaction.options.getString('description'),
+    }
+    const content = [
+      `${options.title}` || `NEW GAME ANNOUNCEMENT`,
+      `${options.date}`,
+      `${options.startTime}-${options.endTime}`,
+      `${options.subtitle}`,
+      `-----------------------`,
+      `Host: ${options.host}`,
+      `Role: @${options.name}`,
+      `-----------------------`,
+      `${options.description}` || '',
+    ].join('\n')
+
+    const row = new ActionRowBuilder<ButtonBuilder>()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('confirm')
+          .setLabel('Cry Havoc')
+          .setStyle(ButtonStyle.Primary),
+      )
+    await interaction.reply({ content, components: [row], ephemeral: true })
   }
 })
 
