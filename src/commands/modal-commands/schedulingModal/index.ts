d00c01a9f6ebe98bbf4b55d7bba8736a5876a61c
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-10-02 00:28:28 -0400

    Cleanup, alpha

--- src/commands/modal-commands/schedulingModal/index.ts
+++ src/commands/modal-commands/schedulingModal/index.ts
@@ -3,11 +3,17 @@ import { createChannels } from './createChannels'
 import { createGameRole } from './createGameRole'
 
 export async function schedulingModal(interaction: ModalSubmitInteraction) {
-  const { guild, fields, member, reply, followUp } = interaction
+  const { guild, fields, member } = interaction
   if (guild == null || member == null) {
     console.error("Guid or Member not found.")
 
     await interaction.reply({ content: "Something went wrong - Guid or Member not found.", ephemeral: true })
+    return
+  }
+
+  const channel = guild.channels.cache.get(process.env.SIGNUP_CHANNEL_ID || 'NO_SIGNUP_CHANNEL_ID')
+  if (!channel?.isTextBased()) {
+    await interaction.reply({ content: "It looks like ERWin can't find the Signup channel.", ephemeral: true })
     return
   }
 
@@ -19,19 +25,18 @@ export async function schedulingModal(interaction: ModalSubmitInteraction) {
   const date = fields.getTextInputValue('date')
   const everyone = guild.roles.everyone
 
-  // Create Role
-  const newRole = await createGameRole(guild, name)
+  if (process.env.SKIP_FOR_DEV) {
+    // Create Role
+    const newRole = await createGameRole(guild, name)
 
-  // Add Role to member who invoked command
-  const memberRoles = member.roles as GuildMemberRoleManager
-  memberRoles.add(newRole)
+    // Add Role to member who invoked command
+    const memberRoles = member.roles as GuildMemberRoleManager
+    memberRoles.add(newRole)
 
-  // Create Channels
-  await createChannels({ guild, name, newRole, everyone, address, startTime, date })
+    // Create Channels
+    await createChannels({ guild, name, newRole, everyone, address, startTime, date })
 
-  // Send Message to Signups channel
-  const channel = guild.channels.cache.get(process.env.SIGNUP_CHANNEL_ID || 'NO_SIGNUP_CHANNEL_ID')
-  if (channel?.isTextBased()) {
+    // Send Message to Signups channel
     await channel.send(name)
   }
 
