6e2a0139b16a0d0c441e656a3cf396836356e38b
alxjrvs@gmail.com <alxjrvs@gmail.com>
2022-10-02 20:12:13 -0400

    no dist

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SignupReactionEmoji = exports.RejectEmoji = exports.MaybeEmoji = exports.ApprovalEmoji = void 0;
-exports.ApprovalEmoji = '✅';
-exports.MaybeEmoji = '❔';
-exports.RejectEmoji = '⛔';
-exports.SignupReactionEmoji = [exports.ApprovalEmoji, exports.MaybeEmoji, exports.RejectEmoji];
